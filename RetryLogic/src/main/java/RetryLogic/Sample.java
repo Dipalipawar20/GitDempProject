3daf2a09f6241cf10e92bf8a86a01db4476a42bf
Dipalipawar20 <bhamare.dipali9@gmail.com>
2022-06-16 13:57:25 +0530

    Commiting the new class

---
+++ RetryLogic/src/main/java/RetryLogic/Sample.java
@@ -0,0 +1,20 @@
+package RetryLogic;
+
+import org.testng.annotations.Test;
+
+public class Sample 
+{
+	
+	@Test
+	public void firstTest()
+	{
+		System.out.println("This is my first test");
+	}
+	
+	@Test
+	public void second()
+	{
+		System.out.println("This is my second test");
+	}
+
+}
